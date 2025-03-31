5deb76e388a5d24dea60c89f9fcf2d82d089fdcf
Davi-Queiroz <162523740+Davi-Queiroz@users.noreply.github.com>
2025-03-31 20:49:08 -0300

    Delete src/Main.java

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Fornecedor fornecedor = new Fornecedor("Pedro Henrique", 777);
-        System.out.println(fornecedor.getNome());
-        fornecedor.setNome("Matosaleu");
-        System.out.println(fornecedor.getNome());
-    }
-}
